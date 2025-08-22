0b51b022a2a8466f696e0eb004815ccacb412efb
Bhavana k m <bhavanakm2004@gmail.com>
2025-08-22 16:15:11 +0530

    first commit

---
+++ sam.py
@@ -0,0 +1,18 @@
+list=['sa','anu','ngh']
+print(list)
+
+location= (13.11,12.44)
+print(location)
+
+fruits={"apple","apple","orange","fdhsgf"}
+print(fruits)
+
+student_record={
+    "anu":{"rollno":1,"marks":{"maths":67,"sci":23}}
+}
+print(student_record)
+
+print(list[2])
+list.append("neha")
+print(list)
+print(len(list))
